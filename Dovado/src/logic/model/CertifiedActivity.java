212d2215d70bd4b2705f406fdcc7b3b756d09f85
Sav <inglip22@gmail.com>
2021-09-04 11:18:41 +0200

    Removed todo from certified activity

--- Dovado/src/logic/model/CertifiedActivity.java
+++ Dovado/src/logic/model/CertifiedActivity.java
@@ -7,17 +7,17 @@ public class CertifiedActivity extends SuperActivity implements Activity {
 
 	public CertifiedActivity(String nome, SuperUser c, Place p, LocalTime openingTime, LocalTime closingTime) {
 		super(nome, c, p,openingTime,closingTime);
-		// TODO Auto-generated constructor stub
+		
 	}
-	
+
 	public CertifiedActivity(String nome, SuperUser c, Place p, LocalTime openingTime, LocalTime closingTime, LocalDate startDate, LocalDate endDate) {
 		super(nome, c, p,openingTime,closingTime,startDate,endDate);
-		// TODO Auto-generated constructor stub
+		
 	}
-	
+
 	public CertifiedActivity(String nome, SuperUser c, Place p, LocalTime openingTime, LocalTime closingTime, LocalDate startDate, LocalDate endDate,Cadence cadence) {
 		super(nome, c, p,openingTime,closingTime,startDate,endDate, cadence);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
