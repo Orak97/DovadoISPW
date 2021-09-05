83f3d9bcc9305865e964b2fbd1773a097b39201e
Andreapagus <34506169+Andreapagus@users.noreply.github.com>
2021-09-05 20:06:17 +0200

    Modificato Dovado.css e reso funzionante EventsView

--- Dovado/src/logic/view/EventsView.java
+++ Dovado/src/logic/view/EventsView.java
@@ -17,6 +17,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -95,6 +96,8 @@ public class EventsView implements Initializable{
 		schedActivities = new ArrayList<ScheduledActivity>();
     	activities = new ArrayList<SuperActivity>();
 		
+    	searchBtn.getStyleClass().add("src-btn");    	
+    	
     	Log.getInstance().logger.info("Ok \nWorking Directory = " + System.getProperty("user.dir"));		
 		try{
 			//Apro di default la lista di attività schedulate.
@@ -106,7 +109,6 @@ public class EventsView implements Initializable{
 
 			for(int j=0;j<activities.size();j++)
 			Log.getInstance().logger.info("tutte le attivit� "+activities.get(j).getId());
-			
 			Thread newThread = new Thread(() -> {
 				int i;
 				for(i=0;i<activities.size();i++) {
@@ -116,24 +118,41 @@ public class EventsView implements Initializable{
 					Text eventInfo = new Text(activities.get(i).getPlace().getName()+
 							"\n"+activities.get(i).getFrequency().getOpeningTime()+
 							"-"+activities.get(i).getFrequency().getClosingTime());
-					eventImage.setImage(new Image("https://source.unsplash.com/user/erondu/200x100"));
+					
+					Button deleteSched = new Button();
+
+					HBox eventLine = new HBox();
+					eventLine.setAlignment(Pos.CENTER);
+					VBox eventButtons = new VBox();
+					
+					eventButtons.getChildren().add(deleteSched);
+					eventButtons.setAlignment(Pos.CENTER_RIGHT);
+					
+					deleteSched.setText("Delete from schedule");
+					deleteSched.getStyleClass().add("src-btn");
+					deleteSched.setAlignment(Pos.CENTER);
+					
+					String width = Double.toString(root.getWidth()/2);
+					String height = Double.toString(root.getHeight()/2);
+					
+					eventImage.setImage(new Image("https://source.unsplash.com/user/erondu/400x100"));
 					eventImage.getStyleClass().add("event-image");
 					
 					eventInfo.setId("eventInfo");
 					eventInfo.getStyleClass().add("textEventInfo");
-					eventInfo.setTextAlignment(TextAlignment.LEFT);
+			/*		eventInfo.setTextAlignment(TextAlignment.LEFT);
 					eventInfo.setFont(Font.font("Monserrat-Black", FontWeight.EXTRA_LIGHT, 12));
 					eventInfo.setFill(Paint.valueOf("#ffffff"));
 					eventInfo.setStrokeWidth(0.3);
 					eventInfo.setStroke(Paint.valueOf("#000000"));
-					
+			*/		
 					eventName.setId("eventName");
 					eventName.getStyleClass().add("textEventName");
-					eventName.setFont(Font.font("Monserrat-Black", FontWeight.BLACK, 20));
+			/*		eventName.setFont(Font.font("Monserrat-Black", FontWeight.BLACK, 20));
 					eventName.setFill(Paint.valueOf("#ffffff"));
 					eventName.setStrokeWidth(0.3);
 					eventName.setStroke(Paint.valueOf("#000000"));
-					
+				*/	
 					VBox eventText = new VBox(eventName,eventInfo);
 					eventText.setAlignment(Pos.CENTER);
 					eventText.getStyleClass().add("eventTextVbox");
@@ -141,6 +160,7 @@ public class EventsView implements Initializable{
 					//informazioni; uso uno StackPane per poter mettere scritte su immagini.
 					StackPane eventBox = new StackPane();
 					eventBox.getStyleClass().add("eventBox");
+					
 					
 					Text eventId = new Text();
 					Text placeId = new Text();
@@ -155,9 +175,11 @@ public class EventsView implements Initializable{
 					eventBox.getChildren().add(eventImage);
 					eventBox.getChildren().add(eventText);
 					//Stabilisco l'allineamento ed in seguito lo aggiungo alla lista di eventi.
-					eventBox.setAlignment(Pos.CENTER);
+					eventBox.setAlignment(Pos.CENTER_LEFT);
 					
-					eventsList.getItems().add(eventBox);
+					eventLine.getChildren().addAll(eventBox,eventButtons);
+					eventLine.setMinWidth(root.getWidth());
+					eventsList.getItems().add(eventLine);
 				}
 			});
 			newThread.start();
@@ -197,29 +219,49 @@ public class EventsView implements Initializable{
 					Text eventInfo = new Text(activities.get(i).getPlace().getName()+
 							"\n"+activities.get(i).getFrequency().getOpeningTime()+
 							"-"+activities.get(i).getFrequency().getClosingTime());
-					eventImage.setImage(new Image("https://source.unsplash.com/user/erondu/200x100"));
-			
+					Button deleteSched = new Button();
+
+					HBox eventLine = new HBox();
+					eventLine.setAlignment(Pos.CENTER);
+					VBox eventButtons = new VBox();
+					
+					eventButtons.getChildren().add(deleteSched);
+					eventButtons.setAlignment(Pos.CENTER_RIGHT);
+					
+					deleteSched.setText("Delete from schedule");
+					deleteSched.getStyleClass().add("src-btn");
+					deleteSched.setAlignment(Pos.CENTER);
+					
+					String width = Double.toString(root.getWidth()/2);
+					String height = Double.toString(root.getHeight()/2);
+					
+					eventImage.setImage(new Image("https://source.unsplash.com/user/erondu/400x100"));
+					eventImage.getStyleClass().add("event-image");
+					
 					eventInfo.setId("eventInfo");
 					eventInfo.getStyleClass().add("textEventInfo");
-					eventInfo.setFont(Font.font("Monserrat-Black", FontWeight.MEDIUM, 12));
-					eventInfo.setTextAlignment(TextAlignment.LEFT);
+			/*		eventInfo.setTextAlignment(TextAlignment.LEFT);
+					eventInfo.setFont(Font.font("Monserrat-Black", FontWeight.EXTRA_LIGHT, 12));
 					eventInfo.setFill(Paint.valueOf("#ffffff"));
+					eventInfo.setStrokeWidth(0.3);
 					eventInfo.setStroke(Paint.valueOf("#000000"));
-			
+			*/		
 					eventName.setId("eventName");
 					eventName.getStyleClass().add("textEventName");
-					eventName.setFont(Font.font("Monserrat-Black", FontWeight.BLACK, 20));
+			/*		eventName.setFont(Font.font("Monserrat-Black", FontWeight.BLACK, 20));
 					eventName.setFill(Paint.valueOf("#ffffff"));
+					eventName.setStrokeWidth(0.3);
 					eventName.setStroke(Paint.valueOf("#000000"));
-			
-					
-					
+				*/	
 					VBox eventText = new VBox(eventName,eventInfo);
 					eventText.setAlignment(Pos.CENTER);
-					
+					eventText.getStyleClass().add("eventTextVbox");
 					//Preparo un box in cui contenere il nome dell'attivit� e altre sue
 					//informazioni; uso uno StackPane per poter mettere scritte su immagini.
 					StackPane eventBox = new StackPane();
+					eventBox.getStyleClass().add("eventBox");
+					
+					
 					Text eventId = new Text();
 					Text placeId = new Text();
 					
@@ -232,11 +274,12 @@ public class EventsView implements Initializable{
 					eventBox.getChildren().add(placeId);
 					eventBox.getChildren().add(eventImage);
 					eventBox.getChildren().add(eventText);
-					
 					//Stabilisco l'allineamento ed in seguito lo aggiungo alla lista di eventi.
-					eventBox.setAlignment(Pos.CENTER);
+					eventBox.setAlignment(Pos.CENTER_LEFT);
 					
-					eventsList.getItems().add(eventBox);
+					eventLine.getChildren().addAll(eventBox,eventButtons);
+					eventLine.setMinWidth(root.getWidth());
+					eventsList.getItems().add(eventLine);
 					}
 				}
 			}
