9cc791c6abcc21b40b155267199fafbea07ff5fc
Orak97 <giovacip97@gmail.com>
2021-09-05 17:59:17 +0200

    Solved code smell on Place

--- Dovado/src/logic/model/DAOPlace.java
+++ Dovado/src/logic/model/DAOPlace.java
@@ -13,10 +13,16 @@ public class DAOPlace {
 	private static DAOPlace INSTANCE;
 	private DAOSuperUser daoSu;
 	private static final  String PLACEJSON = "WebContent/places.json" ;
-	private static final  String COUPONSKEY = "coupons";
-	private static final  String PARTNERKEY = "partner";
-	private static final  String DISCOUNTKEY = "discount";
-	
+	private static final  String PLACESKEY = "places";
+	private static final  String NAMEKEY = "name";
+	private static final  String REGIONKEY = "region";
+	private static final  String CITYKEY = "city";
+	private static final  String ADDRESSKEY = "address";
+	private static final  String CIVICOKEY = "civico";
+	private static final  String ACTIVITIESKEY = "activities";
+	private static final  String IDKEY = "id";
+	private static final  String OWNERKEY = "owner";
+
 	
 	
 	private DAOPlace() {
@@ -38,30 +44,26 @@ public class DAOPlace {
 		daoSu = DAOSuperUser.getInstance();
 		try 
 		{
-			Object places = parser.parse(new FileReader("WebContent/places.json"));
+			Object places = parser.parse(new FileReader(PLACEJSON));
 			JSONObject place = (JSONObject) places;
-			JSONArray placeArray = (JSONArray) place.get("places");
+			JSONArray placeArray = (JSONArray) place.get(PLACESKEY);
 			JSONObject result;
 
 			for(i=0;i<placeArray.size();i++) 
 			{
 				result = (JSONObject)placeArray.get(i);
 				
-				Long idJSON = (Long) result.get("id");
+				Long idJSON = (Long) result.get(IDKEY);
 				
 				if (idJSON.equals(id)) {
-					Place placeFound = new Place((String)result.get("name"),(String) result.get("address"),(String)result.get("city"),(String)result.get("region"),(String) result.get("civico"),(Partner) daoSu.findSuperUserByID((Long)result.get("owner")));
-					placeFound.setId((Long) result.get("id"));
+					Place placeFound = new Place((String)result.get(NAMEKEY),(String) result.get(ADDRESSKEY),(String)result.get(CITYKEY),(String)result.get(REGIONKEY),(String) result.get(CIVICOKEY),(Partner) daoSu.findSuperUserByID((Long)result.get(OWNERKEY)));
+					placeFound.setId((Long) result.get(IDKEY));
 					return placeFound;
 				}
 				
 			}
 			
-		} 
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return null;}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 			}
@@ -74,32 +76,28 @@ public class DAOPlace {
 		daoSu = DAOSuperUser.getInstance();
 		try 
 		{
-			Object places = parser.parse(new FileReader("WebContent/places.json"));
+			Object places = parser.parse(new FileReader(PLACEJSON));
 			JSONObject place = (JSONObject) places;
-			JSONArray placeArray = (JSONArray) place.get("places");
+			JSONArray placeArray = (JSONArray) place.get(PLACESKEY);
 			JSONObject result;
 
 			for(i=0;i<placeArray.size();i++) 
 			{
 				result = (JSONObject)placeArray.get(i);
 				
-				String namePrint = (String) result.get("name");
-				String cityPrint = (String) result.get("city");
-				String regionPrint = (String) result.get("region");
+				String namePrint = (String) result.get(NAMEKEY);
+				String cityPrint = (String) result.get(CITYKEY);
+				String regionPrint = (String) result.get(REGIONKEY);
 				
 				if (name.equals(namePrint) && city.equals(cityPrint) && region.equals(regionPrint)) {
-					Place placeFound = new Place(namePrint,(String) result.get("address"),cityPrint,regionPrint,(String) result.get("civico"),(Partner) daoSu.findSuperUserByID((Long)result.get("owner")));
-					placeFound.setId((Long) result.get("id"));
+					Place placeFound = new Place(namePrint,(String) result.get(ADDRESSKEY),cityPrint,regionPrint,(String) result.get(CIVICOKEY),(Partner) daoSu.findSuperUserByID((Long)result.get(OWNERKEY)));
+					placeFound.setId((Long) result.get(IDKEY));
 					return placeFound;
 				}
 				
 			}
 			
-		} 
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return null;}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 			}
@@ -108,55 +106,52 @@ public class DAOPlace {
 	
 	
 	public int addPlaceToJSON(String address, String name, String city, String region,String civico, Partner owner) {
-		int i,id=0;
+		int i;
+		int id=0;
+		
 		JSONParser parser = new JSONParser();
 		JSONObject newPlace = new JSONObject();
 		JSONArray newPlaceActivities = new JSONArray();
 		
-		newPlace.put("address", address);
-		newPlace.put("civico", civico);
-		newPlace.put("name", name);
-		newPlace.put("city", city);
-		newPlace.put("region", region);
-		newPlace.put("activities", newPlaceActivities);
+		newPlace.put(ADDRESSKEY, address);
+		newPlace.put(CIVICOKEY, civico);
+		newPlace.put(NAMEKEY, name);
+		newPlace.put(CITYKEY, city);
+		newPlace.put(REGIONKEY, region);
+		newPlace.put(ACTIVITIESKEY, newPlaceActivities);
 		if(owner!=null)
-			newPlace.put("owner", owner.getUserID());
+			newPlace.put(OWNERKEY, owner.getUserID());
 		else 
-			newPlace.put("owner", null); 
+			newPlace.put(OWNERKEY, null); 
 		
-		try 
+		try (FileWriter file = new FileWriter(PLACEJSON))
 		{
-			Object places = parser.parse(new FileReader("WebContent/places.json"));
+			Object places = parser.parse(new FileReader(PLACEJSON));
 			JSONObject place = (JSONObject) places;
-			JSONArray placeArray = (JSONArray) place.get("places");
+			JSONArray placeArray = (JSONArray) place.get(PLACESKEY);
 			JSONObject result;
 			
 			for(i=0;i<placeArray.size();i++) 
 			{
 				result = (JSONObject)placeArray.get(i);
 
-				String addressPrint = (String) result.get("address");
-				String civicoPrint = (String) result.get("civico");
-				String namePrint = (String) result.get("name");
-				String cityPrint = (String) result.get("city");
-				String regionPrint = (String) result.get("region");
+				String addressPrint = (String) result.get(ADDRESSKEY);
+				String civicoPrint = (String) result.get(CIVICOKEY);
+				String namePrint = (String) result.get(NAMEKEY);
+				String cityPrint = (String) result.get(CITYKEY);
+				String regionPrint = (String) result.get(REGIONKEY);
 				
 				if(address.equals(addressPrint) && civico.equals(civicoPrint) && name.equals(namePrint) && city.equals(cityPrint) && region.equals(regionPrint))
-					return ((Long) result.get("id")).intValue();
+					return ((Long) result.get(IDKEY)).intValue();
 			}
 			id=i;
-			newPlace.put("id", id);
+			newPlace.put(IDKEY, id);
 			placeArray.add(newPlace);
 			
-			FileWriter file = new FileWriter("WebContent/places.json");
-			file.write(place.toString());;
+			
+			file.write(place.toString());
 			file.flush();
-			file.close();
-		} 
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return -1;}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return -1;
 			}
@@ -170,17 +165,17 @@ public class DAOPlace {
 		int i;
 		JSONParser parser = new JSONParser();
 		
-		try {
-			Object places = parser.parse(new FileReader("WebContent/places.json"));
+		try (FileWriter file = new FileWriter(PLACEJSON)) {
+			Object places = parser.parse(new FileReader(PLACEJSON));
 			JSONObject place = (JSONObject) places;
-			JSONArray placeArray = (JSONArray) place.get("places");
+			JSONArray placeArray = (JSONArray) place.get(PLACESKEY);
 			JSONObject result;
 			
 			for(i=0;i<placeArray.size();i++) 
 			{
 				result = (JSONObject)placeArray.get(i);
 
-				Long placeId = ((Long) result.get("id"));
+				Long placeId = ((Long) result.get(IDKEY));
 				
 				/*--------------------PRINT DI CONTROLLO-----------------------------------------------*/
 				Log.getInstance().logger.info("Controllo di comparazione ID posti:");
@@ -190,19 +185,16 @@ public class DAOPlace {
 				
 				if(p.getId()==(placeId)) {
 					//Salvo nella persistenza il proprietario con il suo nome. In futuro lo si potrebbe salvare in base all'id.
-					result.put("owner", p.getOwner().getUserID());
+					result.put(OWNERKEY, p.getOwner().getUserID());
 					placeArray.set(i, result);
 					break;
 				}
 			}
-			FileWriter file = new FileWriter("WebContent/places.json");
-			file.write(place.toString());;
+			
+			file.write(place.toString());
 			file.flush();
-			file.close();
-		}catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return false;}
-		catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 			}
