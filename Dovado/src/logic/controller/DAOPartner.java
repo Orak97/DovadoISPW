2187ca8072c1159b777094f3574d1d63d93b672d
Sav <inglip22@gmail.com>
2022-01-22 20:02:42 +0100

    Aggiunto metodo per login su DAOPartner

--- Dovado/src/logic/controller/DAOPartner.java
+++ Dovado/src/logic/controller/DAOPartner.java
@@ -7,6 +7,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import logic.model.Partner;
+import logic.model.Preferences;
+import logic.model.User;
 
 public class DAOPartner {
 	
@@ -119,6 +121,76 @@ public class DAOPartner {
                 se.printStackTrace();
             }
         }
+	}
+	
+	public Partner login(String email,String password) throws Exception {
+		// STEP 1: dichiarazioni
+        CallableStatement stmt = null;
+        Connection conn = null;
+        
+        Partner partner = null;
+        
+        try {
+        	
+        	
+        	// STEP 2: loading dinamico del driver mysql
+            Class.forName(DRIVER_CLASS_NAME);
+            
+            // STEP 3: apertura connessione
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            System.out.println("Connected database successfully...");
+            
+            //STEP4.1: preparo la stored procedure
+            String call =  "{call login_partner(?,?)}";
+            
+            stmt = conn.prepareCall(call);
+            
+            stmt.setString(1,email);
+            stmt.setString(2, password);
+            
+            if(!stmt.execute()) {
+            	System.out.println("Partner non trovato");
+            };
+            
+            
+            ResultSet rs = stmt.getResultSet();
+            
+            
+            while(rs.next()) {
+	            partner = new Partner(
+	            			rs.getString("username"),
+	            			rs.getString("email"),
+	            			rs.getLong("idPartner"),
+	            			rs.getString("nome_azienda"),
+	            			rs.getString("partita_iva")
+	            		  );
+            }
+            
+            
+            
+            rs.close();
+            
+            
+        }
+        finally {
+            // STEP 5.2: Clean-up dell'ambiente
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            	throw(se2);
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+                	System.out.println("Disconnetted database successfully...");
+                	
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+           
+        }
+        return partner;
 	}
 
 }
