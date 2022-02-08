026fac9a7e15f389cbb683b3193b386257fbcb25
drakora <drakora@drakora-X556UJ>
2022-02-08 11:01:01 +0100

    really fixed Factory

--- Dovado/src/logic/model/Factory.java
+++ Dovado/src/logic/model/Factory.java
@@ -3,32 +3,36 @@ package logic.model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import logic.controller.ActivityType;
+
 
 public class Factory {
 	private Factory(){}
 	private static Activity newActivity;
-	private static LocalTime[] openCloseTime;
-	private static LocalDate[] startEndDate;
+
+	private static LocalTime[] openCloseTime = {null, null};
+	private static LocalDate[] startEndDate = {null, null};
 	
 	private static void setLocalArray(CreateActivityBean bean) {
-		openCloseTime[0]= bean.getOpeningLocalTime();
-		openCloseTime[1]= bean.getClosingLocalTime();
-		startEndDate[0] = bean.getOpeningLocalDate();
-		startEndDate[1] = bean.getEndLocalDate();
+		if(bean.getType() != ActivityType.CONTINUA) {
+			
+			openCloseTime[0]= bean.getOpeningLocalTime();
+			openCloseTime[1]= bean.getClosingLocalTime();
+			startEndDate[0] = bean.getOpeningLocalDate();
+			startEndDate[1] = bean.getEndLocalDate();
+		}
 	}
 	
 	public static Activity createNormalActivity(Long id,CreateActivityBean bean,Place p) {
-		
+		setLocalArray(bean);
 		switch (bean.getType()) {
 		case CONTINUA:
 			newActivity = new NormalActivity(id, bean.getActivityName(), bean.getActivityDescription(), p, bean.getOpeningLocalTime(), bean.getClosingLocalTime());
 			break;
 		case PERIODICA:
-			setLocalArray(bean);
 			newActivity = new NormalActivity(id,bean.getActivityName(), bean.getActivityDescription(), p, openCloseTime ,startEndDate,bean.getCadence());
 			break;
 		case SCADENZA:
-			setLocalArray(bean);
 			newActivity = new NormalActivity(id,bean.getActivityName(), bean.getActivityDescription(), p, openCloseTime ,startEndDate);
 			break;
 		}
@@ -36,16 +40,15 @@ public class Factory {
 	}
 	
 	public static Activity createCertifiedActivity(Long id,CreateActivityBean bean,Place p, Partner owner) {
+		setLocalArray(bean);
 		switch (bean.getType()) {
 		case CONTINUA:
 			newActivity = new CertifiedActivity(id, bean.getActivityName(), bean.getActivityDescription(), p, bean.getOpeningLocalTime(), bean.getClosingLocalTime(), owner);
 			break;
 		case PERIODICA:
-			setLocalArray(bean);
 			newActivity = new CertifiedActivity(id,bean.getActivityName(), bean.getActivityDescription(), p, openCloseTime ,startEndDate,bean.getCadence(), owner);
 			break;
 		case SCADENZA:
-			setLocalArray(bean);
 			newActivity = new CertifiedActivity(id,bean.getActivityName(), bean.getActivityDescription(), p, openCloseTime ,startEndDate, owner);
 			break;
 		}
