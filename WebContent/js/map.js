49bc0d995027b200795ef370de787db3dae5a013
Andreapagus <a.paganelli997@gmail.com>
2022-02-09 19:15:55 +0100

    Update map.js

--- WebContent/js/map.js
+++ WebContent/js/map.js
@@ -9,7 +9,7 @@ var markersOnMap = [];
 
 var userIcon = L.icon({
 	iconUrl: 'logo/User.png',
-	
+
 	iconSize: [90,95],
 	iconAnchor: [22, 94],
 });
@@ -19,14 +19,14 @@ function startup(userLat, userLong){
 	lat = userLat;
 	long = userLong
 	mymap= L.map('Map').setView([lat, long],zoom);   // Do vita alla mappa partendo dalla latitudine e longitudine [x,y] con uno zoom di 5.
-	
+
 	const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}); // Utilizzo le tiles di openstreetmap ponendo un link al sito (questione di copyright).
 	tiles.addTo(mymap);
-	
+
 	//Infine aggiungo le tiles alla mappa rendendola visibile e interagibile.
-	
+
 	//var cerchio = L.circle([lat,long-0.01], {
 	//  color: 'red',
 	//  fillcolor: '#f03',
@@ -34,7 +34,7 @@ function startup(userLat, userLong){
 	//  radius: 200
 	//}).addTo(mymap);
 	//cerchio.bindPopup("<b>Evento pero' in cerchio</b>").openPopup();
-	
+
 	//var polygon = L.polygon([
 	//  [lat+0.09,long-0.18],
 	//  [lat+0.09,long-0.09],
@@ -42,19 +42,19 @@ function startup(userLat, userLong){
 	//  [lat-0.09,long-0.09]
 	//]).addTo(mymap);
 	//polygon.bindPopup("<b>Evento in un poligono</b><br>br<br>br<br>br").openPopup();
-	
+
 	var searchControl = L.esri.Geocoding.geosearch().addTo(mymap);
-	
+
 	var results = new L.LayerGroup().addTo(mymap);
-	
+
 	searchControl.on('results',function(data){
 		results.clearLayers();
 		for(var i = data.results.length - 1; i >= 0; i--){
 			results.addLayer(L.marker(data.results[i].latlng));
 	  }
 	});
-	
-	
+
+
 
 }
 
@@ -79,7 +79,7 @@ function spotPlace(latitude,longitude,placeName,id){
   var marker = L.marker([latitude,longitude]).addTo(mymap);    //aggiungo un marker per indicare un posto speciale
   marker.bindPopup(placeName);
   markersOnMap.push({'id': id, 'marker': marker});
-  
+
   //e gli aggiungo un pop-up per indicare magari
   //un evento speciale o quel cazzo che ti pare.
 }
@@ -97,47 +97,44 @@ function moveView(latitude,longitude,id){
 }
 
 function removeAllMarkers(){
-	markersOnMap.forEach( elem => elem.marker.remove());  
+	markersOnMap.forEach( elem => elem.marker.remove());
 }
 
 
 //------------------------------------------------------------------------------------------_//
 //								 inizio codice per andrea
 //-------------------------------------------------------------------------------------------//
-	
-	var latDesk;
-	var longDesk;
-	
+
+	var stringedLatLng
+
 	//chiama prima questa
-  	function retrieveLatLng(civicoTxt,indirizzoTxt,cittaTxt,regioneTxt,capTxt){
-		
-  		L.esri.Geocoding.geocode().address(civicoTxt+' '+indirizzoTxt).city(cittaTxt).region(regioneTxt).postal(capTxt).run( function (err, results, response) {
+  	function retrieveLatLng(civicoTxt,indirizzoTxt,cittaTxt,regioneTxt){
+
+  		L.esri.Geocoding.geocode().address(civicoTxt+' '+indirizzoTxt).city(cittaTxt).region(regioneTxt).run( function (err, results, response) {
   		  if (err) {
 //  		    console.log(err);
   		    return;
   		  }
-  		  
+
   		  if(results.results.length < 1){
-  			 desktopLatLng = null
+  			 latLng = null
+				 longDesk = null
   		  }else{
-  		  
-	  		  //console.log(results.results)
-	  		  
+
+					let latStr = (results.results[0].latlng.lat).toString()
+					let lonStr = (results.results[0].latlng.lng).toString()
+
+					stringedLatLng = latStr.concat(";",lonStr)
+
+
 	  		  //let latitude = results.results[0].latlng.lat;
 	  		  //let longitude = results.results[0].latlng.lng
 	  		  //console.log(results.results[0].latlng);
-	  		  latDesk = results.results[0].latlng.lat;
-	  		  longDesk = results.results[0].latlng.lnt;
   		  }
   		});
   	}
-  	
+
   	//e poi queste
-  	function getDesktopLatitude(){
-		return latDesk;
-	}
-	
-	//e poi questa
-  	function getDesktopLongitude(){
-		return longDesk;
+  	function getStringedLatLng(){
+		return stringedLatLng;
 	}
