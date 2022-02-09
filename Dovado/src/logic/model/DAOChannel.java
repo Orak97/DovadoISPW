a7abac590f50a20238253c7746ca6151afe6c237
drakora <drakora@drakora-X556UJ>
2022-02-09 14:54:05 +0100

    previous commit update

--- Dovado/src/logic/model/DAOChannel.java
+++ Dovado/src/logic/model/DAOChannel.java
@@ -19,19 +19,8 @@ import org.json.simple.parser.ParseException;
 
 import logic.controller.FindActivityController;
 
-public class DAOChannel {
+public class DAOChannel extends DAO{
 	private static DAOChannel INSTANCE;
-
-	//----------database--------------------------------------
-	
-	private static final String USER = "dovado"; //DA CAMBIARE
-	private static final String PASSWORD = "dovadogang"; //DA CAMBIARE
-	private static final String DB_URL = "jdbc:mariadb://localhost:3306/dovado";
-	private static final String DRIVER_CLASS_NAME = "org.mariadb.jdbc.Driver";
-			
-	//----------log message-----------------------------------------------
-	private static final String LOGDBCONN = "Connected database successfully...";
-	private static final String LOGDBDISCONN = "Disconnetted database successfully...";
 		
 	//--------------------------------------------------------------
 		
@@ -109,36 +98,5 @@ public class DAOChannel {
         	//Clean-up dell'ambiente
             disconnRoutine();
         }
-	}
-	private void resetConnection() throws ClassNotFoundException, SQLException {
-		conn = null;
-		stmt = null;
-
-		Class.forName(DRIVER_CLASS_NAME);
-
-		// apertura connessione
-        conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-        Log.getInstance().getLogger().info(LOGDBCONN);
-	}
-	
-	private void disconnRoutine() throws SQLException {
-		
-		try {
-	        if (stmt != null)
-	            stmt.close();
-	    } catch (SQLException se2) {
-	    	Log.getInstance().getLogger().warning("Errore di codice: "+ se2.getErrorCode() + " e mesaggio: " + se2.getMessage());
-	    	se2.printStackTrace();
-	    	throw se2;
-	    }
-	    try {
-	        if (conn != null)
-	            conn.close();
-	    	Log.getInstance().getLogger().info(LOGDBDISCONN);
-
-	    } catch (SQLException se) {
-	    	Log.getInstance().getLogger().warning("Errore di codice: "+ se.getErrorCode() + " e mesaggio: " + se.getMessage());
-	    	se.printStackTrace();
-	    }
 	}
 }
