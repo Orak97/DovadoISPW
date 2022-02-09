1323a0d196c6fdc15d371e97462843f9f89c077b
Orak97 <giovacip97@gmail.com>
2022-02-09 23:01:35 +0100

    Modified Preferences

--- Dovado/src/logic/model/Preferences.java
+++ Dovado/src/logic/model/Preferences.java
@@ -1,20 +1,6 @@
 package logic.model;
 
 public class Preferences {
-	private boolean arte; 
-	private boolean cibo; 
-	private boolean musica;
-	private boolean sport;
-	private boolean social;
-	private boolean natura; 
-	private boolean esplorazione; 
-	private boolean ricorrenzeLocali;
-	private boolean moda;
-	private boolean shopping;
-	private boolean adrenalina;
-	private boolean relax;
-	private boolean istruzione; 
-	private boolean monumenti;
 	
 	private PreferenceBean prefs;
 	
