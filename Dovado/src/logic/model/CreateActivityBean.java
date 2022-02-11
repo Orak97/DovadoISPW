ba93537022c656c2ea519ab600f35eef325185b5
Orak97 <giovacip97@gmail.com>
2022-02-11 03:20:11 +0100

    Solved last code smell

--- Dovado/src/logic/model/CreateActivityBean.java
+++ Dovado/src/logic/model/CreateActivityBean.java
@@ -13,7 +13,7 @@ import logic.controller.ActivityType;
  * @author sav
  *
  */
-public class CreateActivityBean {
+public class CreateActivityBean extends PreferenceBean{
 	
 	private String activityName;
 	private String activityDescription;
@@ -27,21 +27,6 @@ public class CreateActivityBean {
 
 	private ActivityType type;
 	private Cadence cadence;
-	
-	private boolean arte = false;
-	private boolean cibo = false;
-	private boolean musica = false;
-	private boolean sport = false;
-	private boolean social = false;
-	private boolean natura = false;
-	private boolean esplorazione = false;
-	private boolean ricorrenze = false;
-	private boolean moda = false;
-	private boolean shopping = false;
-	private boolean adrenalina = false;
-	private boolean relax = false;
-	private boolean istruzione= false;
-	private boolean monumenti = false;
 	
 	private int idActivity;
 	private int owner = 0;
@@ -160,117 +145,6 @@ public class CreateActivityBean {
 	
 	//-------inizio getter e setter delle preferenze----------------
 	
-	public boolean isArte() {
-		return arte;
-	}
-
-	public void setArte(boolean arte) {
-		this.arte = arte;
-	}
-
-	public boolean isCibo() {
-		return cibo;
-	}
-
-	public void setCibo(boolean cibo) {
-		this.cibo = cibo;
-	}
-
-	public boolean isMusica() {
-		return musica;
-	}
-
-	public void setMusica(boolean musica) {
-		this.musica = musica;
-	}
-
-	public boolean isSport() {
-		return sport;
-	}
-
-	public void setSport(boolean sport) {
-		this.sport = sport;
-	}
-
-	public boolean isSocial() {
-		return social;
-	}
-
-	public void setSocial(boolean social) {
-		this.social = social;
-	}
-
-	public boolean isNatura() {
-		return natura;
-	}
-
-	public void setNatura(boolean natura) {
-		this.natura = natura;
-	}
-
-	public boolean isEsplorazione() {
-		return esplorazione;
-	}
-
-	public void setEsplorazione(boolean esplorazione) {
-		this.esplorazione = esplorazione;
-	}
-
-	public boolean isRicorrenze() {
-		return ricorrenze;
-	}
-
-	public void setRicorrenze(boolean ricorrenze) {
-		this.ricorrenze = ricorrenze;
-	}
-
-	public boolean isShopping() {
-		return shopping;
-	}
-
-	public void setShopping(boolean shopping) {
-		this.shopping = shopping;
-	}
-
-	public boolean isModa() {
-		return moda;
-	}
-
-	public void setModa(boolean moda) {
-		this.moda = moda;
-	}
-
-	public boolean isAdrenalina() {
-		return adrenalina;
-	}
-
-	public void setAdrenalina(boolean adrenalina) {
-		this.adrenalina = adrenalina;
-	}
-
-	public boolean isRelax() {
-		return relax;
-	}
-
-	public void setRelax(boolean relax) {
-		this.relax = relax;
-	}
-
-	public boolean isIstruzione() {
-		return istruzione;
-	}
-
-	public void setIstruzione(boolean istruzione) {
-		this.istruzione = istruzione;
-	}
-
-	public boolean isMonumenti() {
-		return monumenti;
-	}
-
-	public void setMonumenti(boolean monumenti) {
-		this.monumenti = monumenti;
-	}
 
 	public int getPlace() {
 		return place;
