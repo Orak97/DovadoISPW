01c17ad5b09e2265abd2e8205926b032d2284ddd
sav-bit <inglip22@gmail.com>
2022-02-13 11:43:33 +0100

    edited test class for redeeming coupon and generate coupon

--- src/test/java/test/TestAddActivityToScheduleController.java
+++ src/test/java/test/TestAddActivityToScheduleController.java
@@ -1,6 +1,9 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -12,7 +15,9 @@ import logic.controller.AddActivityToScheduleController;
 import logic.model.DAOPartner;
 import logic.model.Log;
 import logic.model.Partner;
+import logic.model.Schedule;
 import logic.model.ScheduleBean;
+import logic.model.ScheduledActivity;
 import logic.model.User;
 
 /*
@@ -24,13 +29,68 @@ public class TestAddActivityToScheduleController {
 	
 	private static final int partnerAuth = 16; //partner proprietario
 	private static final int partnerNotAuth = 22; //partner NON proprietario
-	private static final String couponToRedeem = "560926";
+	private static String couponToRedeem;
 	private static final Long idNormalActivity = 4L;
+	private static final Long idCertifiedActivity = 6L;
 	
-	/* TEST REDEEM COUPON */
+	private ScheduleBean bean;
+	
+	public TestAddActivityToScheduleController() {
+		 bean = new ScheduleBean();
+		 bean.setScheduledDate("2022-03-17");
+		 bean.setScheduledTime("10:12");
+	}
+	
+	/* test Add activity to schedule */
 	
 	@Test
 	@Order(1)
+	void testAddCertifiedActivityToScheduleFailsNotCert() {
+		/*voglio evitare che gli utenti possano aggiungere attività non certificate
+		* allo schedulo come se fossero certificate, quindi ad esempio generando coupon per quest'ultime
+		* che non avrebbero nessuno che le riscatta
+		*/
+		
+		User explorer = new User(null,null,15L,200L);
+		bean.setIdActivity(idNormalActivity); //attività che si fa tutti i fine settimana -> sabato 12 è ok
+		AddActivityToScheduleController controller = new AddActivityToScheduleController(explorer,bean);
+		Assertions.assertThrows(SQLException.class, ()->controller.addCertifiedActivityToSchedule());		
+	}
+	
+	@Test
+	@Order(2)
+	void testAddCertifiedActivityToScheduleSuccess() {
+		/*voglio controllare che gli utenti possano aggiungere attività certificate
+		* allo schedulo, generando un coupon per quest'ultime
+		*/
+		
+		User explorer = new User(null,null,15L,200L);
+		bean.setIdActivity(idCertifiedActivity); //attività che si fa tutti i fine settimana -> sabato 12 è ok
+		AddActivityToScheduleController controller = new AddActivityToScheduleController(explorer,bean);
+		try {
+			controller.addCertifiedActivityToSchedule();
+			Schedule s = explorer.getSchedule();
+			ArrayList<ScheduledActivity> sActivities = (ArrayList<ScheduledActivity>) s.getScheduledActivities();
+			ScheduledActivity mySchedule = null;
+			
+			for(ScheduledActivity curr: sActivities) {
+				if(curr.getReferencedActivity().getId() == idCertifiedActivity && curr.getScheduledTime().isEqual(bean.getScheduledDateTime())) mySchedule = curr;
+			}
+			
+			if(mySchedule != null) couponToRedeem = String.valueOf(mySchedule.getCoupon().getCouponCode());
+			
+			assertNotNull(mySchedule);
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			Log.getInstance().getLogger().info("error durate la generazione del coupon:"+e);
+			e.printStackTrace();
+		}		
+	}
+	
+	
+	
+	@Test
+	@Order(3)
 	void testRedeemCouponFailsNotActivityOwner() {
 		/*Se un partner non è il proprietario dell'attività per cui il coupon è stato generato 
 		 * voglio evitare che sia in grado di riscattarlo
@@ -48,25 +108,26 @@ public class TestAddActivityToScheduleController {
 	}
 	
 	@Test
-	@Order(2)
-	void testAddCertifiedActivityToScheduleFailsNotCert() {
-		/*voglio evitare che gli utenti possano aggiungere attività non certificate
-		* allo schedulo come se fossero certificate, quindi ad esempio generando coupon per quest'ultime
-		* che non avrebbero nessuno che le riscatta
+	@Order(4)
+	void testRedeemCouponSuccessActivityOwner() {
+		/*Se un partner non è il proprietario dell'attività per cui il coupon è stato generato 
+		 * voglio evitare che sia in grado di riscattarlo
 		*/
 		
-		User explorer = new User(null,null,15L,200L);
-		ScheduleBean bean = new ScheduleBean();
-		bean.setScheduledDate("2022-02-12");
-		bean.setScheduledTime("10:12");
-		bean.setIdActivity(idNormalActivity); //attività che si fa tutti i fine settimana -> sabato 12 è ok
-		AddActivityToScheduleController controller = new AddActivityToScheduleController(explorer,bean);
-		Assertions.assertThrows(SQLException.class, ()->controller.addCertifiedActivityToSchedule());		
+		Partner authorizedPartner;
+		try {
+			authorizedPartner = DAOPartner.getInstance().getPartnerInfo(partnerAuth);
+			AddActivityToScheduleController controller = new AddActivityToScheduleController(authorizedPartner,couponToRedeem);
+			Assertions.assertDoesNotThrow(()->controller.redeemCoupon());		
+		} catch (ClassNotFoundException | SQLException e) {
+			Log.getInstance().getLogger().info("error on retrieving the partner :"+e);
+			e.printStackTrace();
+		}		
 	}
 	
 	
 	@Test
-	@Order(3)
+	@Order(5)
 	void testRemoveScheduleFailsNotOwner() {
 		/*voglio evitare che un utente possa cancellare un'attività schedulata 
 		* nel caso in cui lui non sia l'utente proprietario dello schedulo 
